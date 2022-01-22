f038287df9a3a90a3c0f77102d956a95c05c7701
Geraldo Costa <geraldocoosta@gmail.com>
2022-01-22 10:51:51 -0300

    hello full cycle

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Hello World"))
+		w.Write([]byte("Hello World, Full Cycle"))
 	})
 	http.ListenAndServe(":8080", nil )
 }
